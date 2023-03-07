8af54f433fef4792c8433b5f6d45407c7fb0715e
Kzero <hindratmohs@gmail.com>
2023-03-07 10:49:14 +0700

    update

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -1,11 +1,7 @@
-#!/bin/sh
+#!/bin.sh
 
-# Perintah untuk build Docker image dari berkas Dockerfile yang disediakan dengan nama <username-docker>/karsajobs:lates
 docker build -t zero4335/karsajobs-ui:latest .
 
-# Perintah untuk login ke Docker Hub
 echo $PASSWORD_DOCKER_HUB | docker login -u zero4335 --password-stdin
 
-# Perintah untuk push image ke Docker Hub
 docker push zero4335/karsajobs-ui:latest
-
